8fcca08313ccb2fdaff2c6f9de8454bc54b6598d
Martin Fieber <info@martin-fieber.de>
2018-01-14 17:32:10 +0100

    feat: activated google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,13 @@ module.exports = {
                 siteUrl: 'https://martin-fieber.de'
             }
         },
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                trackingId: process.env.TRACKING_ID,
+                anonymize: true
+            }
+        },
         'gatsby-transformer-sharp',
         'gatsby-plugin-sharp',
         'gatsby-plugin-react-helmet',
