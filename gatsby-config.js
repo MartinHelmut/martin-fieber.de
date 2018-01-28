20490c011e9610ad43ac4b36e3270a44664a0263
Martin Fieber <martin.fieber@sinnerschrader.com>
2018-01-28 16:02:37 +0100

    refactor(gatsby): deactivate google fonts loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,8 @@ module.exports = {
         {
             resolve: 'gatsby-plugin-typography',
             options: {
-                pathToConfigModule: 'src/utils/typography.js'
+                pathToConfigModule: 'src/utils/typography.js',
+                omitGoogleFont: true
             }
         },
         {
