9a48098faf04a17fa2d792ff834c22ebd62ee639
Martin Fieber <info@martin-fieber.de>
2018-05-17 16:39:32 +0200

    feat: add basic pages for legal

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import Link from 'next/link';
 
-const Index = () => <div>Welcome to next.js!</div>;
+const Index = () => (
+    <div>
+        <h1>Welcome to next.js!</h1>
+        <Link href="/legalNotice">
+            <a>Legal notice</a>
+        </Link>
+        {' | '}
+        <Link href="/privacyPolicy">
+            <a>Privacy policy</a>
+        </Link>
+    </div>
+);
 
 export default Index;
