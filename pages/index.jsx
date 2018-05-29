14af2ceccabb86b94c859859b8217d687215b2bf
Martin Fieber <info@martin-fieber.de>
2018-05-29 16:45:35 +0200

    fix: routing and link names for pages

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 const Index = () => (
     <div>
         <h1>Welcome to martin-fieber.de!</h1>
-        <Link href="/legalNotice">
+        <Link href="/legal-notice">
             <a>Legal notice</a>
         </Link>
         {' | '}
-        <Link href="/privacyPolicy">
+        <Link href="/privacy-policy">
             <a>Privacy policy</a>
         </Link>
     </div>
