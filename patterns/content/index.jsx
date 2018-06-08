ad7d99dfc728348e66025e78b7ba560ea9d28fb2
Martin Fieber <info@martin-fieber.de>
2018-06-08 16:17:30 +0200

    refactor(patterns): split ui from logic by extracting components

---
+++ patterns/content/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './index.css';
+
+const Content = ({ children }) => (
+    <div className={styles.content}>{children}</div>
+);
+
+export default Content;
