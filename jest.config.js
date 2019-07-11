952341db175cd315e0b6854f91c8cc499e18c15e
Martin Helmut Fieber <info@martin-fieber.de>
2019-07-11 21:28:31 +0200

    chore: use yarn

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: "./coverage",
   coverageReporters: ["text", "lcov"],
-  modulePathIgnorePatterns: ["/.next/", "/public/"],
+  modulePathIgnorePatterns: ["/.next/", "/out/"],
   reporters: ["jest-dot-reporter"],
   verbose: false
 };
