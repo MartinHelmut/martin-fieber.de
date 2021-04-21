7048babc9cc01488fecab171a42b941b1fa6737a
Martin Helmut Fieber <info@martin-fieber.se>
2021-04-21 21:20:48 +0200

    build: use pnpm

--- scripts/ensure-package-manager.js
+++ scripts/ensure-package-manager.js
@@ -1,9 +1,12 @@
 "use strict";
 
-if (!process.env.npm_config_user_agent.startsWith("yarn/")) {
+if (
+  process.env.SKIP_PNPM_CHECK !== "true" &&
+  !process.env.npm_config_user_agent.startsWith("pnpm/")
+) {
   console.log(
     "\x1b[31m%s\x1b[0m",
-    "Error: Use `yarn` to install dependencies in this repository\n"
+    "Error: Use `pnpm` to install dependencies in this repository\n"
   );
   process.exit(1);
 }
